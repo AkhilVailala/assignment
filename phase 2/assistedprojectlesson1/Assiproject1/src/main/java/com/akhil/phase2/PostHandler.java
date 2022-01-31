f5b765870d7bea878b095bba2bae40f231c8f2ae
AkhilVailala <97245932+AkhilVailala@users.noreply.github.com>
2022-01-31 22:35:48 +0530

    Add files via upload

---
+++ phase 2/assistedprojectlesson1/Assiproject1/src/main/java/com/akhil/phase2/PostHandler.java
@@ -0,0 +1,48 @@
+package com.akhil.phase2;
+import java.io.*;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+* Servlet implementation class PostHandler
+*/
+@WebServlet("/PostHandler")
+public class PostHandler extends HttpServlet {
+        private static final long serialVersionUID = 1L;
+       
+    /**
+* @see HttpServlet#HttpServlet()
+*/
+    public PostHandler() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+        /**
+         * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                
+        }
+
+        /**
+         * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+         */
+        protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+                // TODO Auto-generated method stub
+                  PrintWriter out = response.getWriter();
+                    out.println("<html><body>");
+                    
+                    String name = request.getParameter("name");
+                    String  password = request.getParameter(" password");
+                    
+                    out.println("Name=" + name + "<br>Address=" +  password);
+                    out.println("</body></html>");
+        }}
+
