8c356a2199a7230b9860f5c5a5369d25be9f2d7b
AkhilVailala <97245932+AkhilVailala@users.noreply.github.com>
2022-02-01 22:07:30 +0530

    Add files via upload

---
+++ phase 2/Assistedproject lesson2/Asspl22/src/main/java/com/company/Asspl22/App.java
@@ -0,0 +1,13 @@
+package com.company.Asspl22;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
